28669a2d6241dd1ee6be84215e94baf6f66dbb08
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-12-10 12:44:44 +0000

    Create game.py

---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame
+
+SC_WIDTH = 800
+SC_HEIGHT = 600
+
+pygame.init()
+screen = pygame.display.set_mode((SC_WIDTH, SC_HEIGHT))
+pygame.display.set_caption('Cosmic Swarm')
+running = True
+while running:
+    #eventos
+    pass
+
+    #lógica
+    pass
+
+    #gráficos
+    pass
+
+pygame.quit()
