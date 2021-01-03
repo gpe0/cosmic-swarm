2af7639f461b2f644a20819ac304ab5994d7c5ea
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2021-01-03 16:00:30 +0000

    Finishing energized script & Debugging

--- game.py
+++ game.py
@@ -15,13 +15,16 @@ def collision_blocks(p1, r1, p2, r2):
     d = ((p1[0]) - (p2[0] + 6))**2 + ((p1[1]) - (p2[1] + 6))**2
     return (d < (r1 + r2)**2)
 
+def collision_bullets_blocks(p1, r1, p2, r2):
+    d = ((p1[0]) - (p2[0] + 6))**2 + ((p1[1]) - (p2[1] + 6))**2
+    return (d < (r1 + r2)**2)
 
 def collision_bullets(p1, r1, p2, r2, moving, energized):
     d1 = ((p1[0] + 1) - (p2[0] + 20))**2 + ((p1[1] + 1) - (p2[1] + 20))**2
     d2 = ((p1[0] + 1) - (p2[0] + 20))**2 + ((p1[1] + 1) - (p2[1] + 60))**2
     d1_e = ((p1[0] + 1) - (p2[0] + 5))**2 + ((p1[1] + 1) - (p2[1] + 85))**2
     d2_e = ((p1[0] + 1) - (p2[0] + 15))**2 + ((p1[1] + 1) - (p2[1] + 85))**2
-    d3_e = ((p1[0] + 1) - (p2[0] + 25))**2 + ((p1[1] + 1) - (p2[1] + 85))**2
+    d3_e = ((p1[0] + 1) - (p2[0] + 25))**2 + ((p1[1] + 1) - (p2[1] + 5))**2
     d4_e = ((p1[0] + 1) - (p2[0] + 35))**2 + ((p1[1] + 1) - (p2[1] + 85))**2
     if not moving:
         pass
@@ -126,7 +129,7 @@ while running:
                 rot_left = True  
             elif ev.key == pygame.K_e:
                 rot_right = True  
-            elif ev.key == pygame.K_SPACE and len(b_pos_x) == 0:
+            elif ev.key == pygame.K_SPACE:
                 shooting = True
         elif ev.type == pygame.KEYUP:
             if ev.key == pygame.K_a:
@@ -196,17 +199,16 @@ while running:
         t_color_out.append(colors_out[col])
         rand = random.randint(0, 3)
         if rand == 0:
-            leave_pos.append((int(random.random() * 1000),-80))
+            leave_pos.append((int(random.random() * 1000 - 20),-80 - 85))
         elif rand == 1:
-            leave_pos.append((1080,int(random.random() * 800)))
+            leave_pos.append((1080 - 20,int(random.random() * 800)- 85))
         elif rand == 2:
-            leave_pos.append((int(random.random() * 1000),880))
+            leave_pos.append((int(random.random() * 1000)- 20,880- 85))
         else:
-            leave_pos.append((-80,int(random.random() * 800)))
-        place_pos.append((int(random.random() * 1000), int(random.random() * 800)))
+            leave_pos.append((-80 - 20,int(random.random() * 800)- 85))
+        place_pos.append((int(random.random() * 1000)- 20, int(random.random() * 800)- 85))
     for ind in range(len((t_pos_x))):
-        
-        
+
         if moving[ind]:
             t_vel_x = t_vel * dt * (place_pos[ind][0] - t_pos_x[ind])
             t_vel_y = t_vel * dt * (place_pos[ind][1] - t_pos_y[ind])
@@ -260,32 +262,51 @@ while running:
                     del increment[0]
                     break
         
-        #Shooting Logic
+    #Shooting Logic
 
-        if shooting and pygame.time.get_ticks() >= time_shoot and len(b_pos_x) == 0:
-            b_pos_x.append(pos_x + 16 * -math.sin(math.radians(ang)))
-            b_pos_y.append(pos_y + 16 * -math.cos(math.radians(ang)))
-            increment.append((-math.sin(math.radians(ang)), -math.cos(math.radians(ang))))
-            time_shoot += shoot_cool
-            shooting = False
-        
-       
-        for ind in range(len(b_pos_x)):
-            b_vel_x = bul_sp * dt * increment[ind][0]
-            b_vel_y = bul_sp * dt * increment[ind][1]
-            b_pos_x[ind], b_pos_y[ind] = b_pos_x[ind] + b_vel_x, b_pos_y[ind] + b_vel_y
-            if b_pos_x[ind] < 0 or b_pos_x[ind] > SC_WIDTH or b_pos_y[ind] < 0 or b_pos_y[ind] > SC_HEIGHT:
-                del b_pos_x[ind]
-                del b_pos_y[ind]
-                del increment[ind]
+    if shooting and pygame.time.get_ticks() >= time_shoot and len(b_pos_x) == 0:
+        b_pos_x.append(pos_x + 16 * -math.sin(math.radians(ang)))
+        b_pos_y.append(pos_y + 16 * -math.cos(math.radians(ang)))
+        increment.append((-math.sin(math.radians(ang)), -math.cos(math.radians(ang))))
+        time_shoot = pygame.time.get_ticks() + shoot_cool
+        shooting = False
+    
+    
+    for ind in range(len(b_pos_x)):
+        b_vel_x = bul_sp * dt * increment[ind][0]
+        b_vel_y = bul_sp * dt * increment[ind][1]
+        b_pos_x[ind], b_pos_y[ind] = b_pos_x[ind] + b_vel_x, b_pos_y[ind] + b_vel_y
+        if b_pos_x[ind] < 0 or b_pos_x[ind] > SC_WIDTH or b_pos_y[ind] < 0 or b_pos_y[ind] > SC_HEIGHT:
+            del b_pos_x[ind]
+            del b_pos_y[ind]
+            del increment[ind]
+            break
+    for ind in range(len((t_pos_x))):
+        if collision_termites((pos_x, pos_y), r, (t_pos_x[ind], t_pos_y[ind]) , r_t):
+                running = False
                 break
-        for ind in range(len((t_pos_x))):
-            if collision_termites((pos_x, pos_y), r, (t_pos_x[ind], t_pos_y[ind]) , r_t):
-                    running = False
-                    break
-        for b in blocks:
+    for ind, b in enumerate(blocks):
+        if not energized:
             if collision_blocks((pos_x, pos_y), r, b, r_b):
                 running = False
+            if len(b_pos_x) != 0:
+                if collision_bullets_blocks((b_pos_x[0], b_pos_y[0]), 1, b, r_b):
+                        del b_pos_x[0]
+                        del b_pos_y[0]
+                        del increment[0]
+                        break
+        else:
+            if collision_blocks((pos_x, pos_y), r, b, r_b):
+                del blocks[ind]
+                running = False
+                break
+            if len(b_pos_x) != 0:
+                if collision_bullets_blocks((b_pos_x[0], b_pos_y[0]), 1, b, r_b):
+                    del blocks[ind]
+                    del b_pos_x[0]
+                    del b_pos_y[0]
+                    del increment[0]
+                    break
     if energized and pygame.time.get_ticks() - en_time >= en_cool:
         energized = False
 
